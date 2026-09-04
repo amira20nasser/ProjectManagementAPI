c8765509a1e30a7a8d405c5a92dc99cc853b48ae
amira_nasser <amira20nasser@gmail.com>
2026-09-04 12:52:47 +0300

    feat(api): configure Swagger, DI and database connection
    Wire AddInfrastructure in Program.cs and configure Swagger/Swashbuckle with OpenApiInfo and JWT Bearer security scheme
    
    Enable Swagger UI at /swagger in Development and keep MapOpenApi
    
    Add Swashbuckle.AspNetCore v10.2.3 package reference
    
    Add DefaultConnection to appsettings.json (SQL Server, ProjectManagementDB)
    
    Update launchSettings.json to launch browser at swagger with http://localhost:5279

--- ProjectManagementAPI/Program.cs
+++ ProjectManagementAPI/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.OpenApi;
+using ProjectManagement.Infrastructure;
+
 namespace ProjectManagementAPI
 {
     public class Program
@@ -10,8 +13,40 @@ namespace ProjectManagementAPI
             // Add services to the container.
 
             builder.Services.AddControllers();
+            // Infrastructure - Database, Repositories, UnitOfWork
+            builder.Services.AddInfrastructure(builder.Configuration);
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+
+            builder.Services.AddSwaggerGen(
+                options =>
+                {
+                    options.SwaggerDoc("v1", new OpenApiInfo
+                    {
+                        Title = "Project Management API",
+                        Version = "v1",
+                        Description = "Project Management API Documentation"
+                    });
+
+                    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                    {
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.Http,
+                        Scheme = "bearer",
+                        BearerFormat = "JWT",
+                        In = ParameterLocation.Header
+                    });
+
+                    options.AddSecurityRequirement((document) => new OpenApiSecurityRequirement
+                {
+                    {
+                       new OpenApiSecuritySchemeReference("Bearer",document),
+                       []
+                    }
+                });
+                }
+                
+                );
 
             var app = builder.Build();
 
@@ -19,6 +54,12 @@ namespace ProjectManagementAPI
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Project Management API v1");
+                    options.RoutePrefix = "swagger";
+                });
             }
 
             app.UseHttpsRedirection();
